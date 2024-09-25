ee765d77b9a3f2828ad0fc102fc118601fbfbcfe
sunsuking <sunsuking@gmail.com>
2024-09-25 18:00:23 +0900

    블록체인 코드 수정

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -7,7 +7,7 @@ import useWeb3 from "@/hooks/useWeb3";
 import axios from "axios";
 
 export default function ProjectPage({ params }: { params: { id: string } }) {
-  const { account, amount, connectWallet, signTransaction } = useWeb3();
+  const { account, amount, connectWallet, signTransaction, signToSend, createSourceCodeToken } = useWeb3();
 
   useEffect(() => {
     connectWallet();
@@ -17,18 +17,30 @@ export default function ProjectPage({ params }: { params: { id: string } }) {
   return (
     <Suspense fallback={<SkeletonUI />}>
       <button onClick={async () => {
-        const transaction = await signTransaction("0x2Ec4c72bEdCdEa67776AF695B52386c264e6b0Dd");
+        const transaction = await signTransaction("0x25D4BCE86828E434e99fc42B2C74fdEe98F5c91e");
         axios.post("http://127.0.0.1:5555/payment/signature", {
           amount: amount,
           deploymentId: 1,
           transaction: transaction,
-          receipientId: 3,
+          receipientId: 5,
         }, {
           headers: {
-            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3IiwiYXV0aG9yaXRpZXMiOiJQUk9WSURFUiIsImlhdCI6MTcyNjU3MTA0OSwiZXhwIjoxNzI2NTcxMTA5fQ.2VHBljonWGklhv4oAPuv7_yWfbX2Uipki3BUSorGoYM`
+            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4IiwiYXV0aG9yaXRpZXMiOiJQUk9WSURFUiIsImlhdCI6MTcyNzIyOTAxNiwiZXhwIjoxNzI5MjI5MDE2fQ.CkDerOFNyp4IJzTRME5dHqaZrAieIEIdrCyv1KyyZB0`
           }
         })
       }}>test</button>
+
+      <button  onClick={async () => signToSend()}>사인</button>
+
+      <button onClick={async () => createSourceCodeToken(
+        "project name",
+        "20a99b048b84897fbf673d363ee3831adb891c0b",
+        1,
+        [
+          "20a99b048b84897fbf673d363ee3831adb891c0b",
+          "20a99b048b84897fbf673d363ee3831adb891c0b"
+        ]
+      )}>민팅 테스트</button>
       {/* <ProjectContent id={params.id} /> */}
     </Suspense>
   );
--- client/src/hooks/useWeb3.tsx
+++ client/src/hooks/useWeb3.tsx
@@ -2,6 +2,8 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import PaymentContract from "@/contracts/PaymentContract.json";
+import SourceCodeToken from "@/contracts/SourceCodeToken.json"
+import ERC20 from "@/contracts/ERC20.json"
 import Web3 from "web3";
 
 declare global {
@@ -10,11 +12,11 @@ declare global {
   }
 }
 
-const TRANSACTION_GAS_LIMIT = 300000;
-const MAX_FEE_PER_GAS = 20000000000;
+const GAS_PRICE = 0;
+const GAS_LIMIT = 50000;
 
 const useWeb3 = () => {
-  const amount = 0.0001;
+  const amount = 10;
   const [account, setAccount] = useState<string | undefined>();
   const [provider, setProvider] = useState<ethers.BrowserProvider | null>(null);
 
@@ -40,37 +42,79 @@ const useWeb3 = () => {
     }
   };
 
+  async function signToSend(): Promise<void>{
+    if (!provider) {
+      throw new Error("블록체인 서버에 연결이 되어있지 않습니다.");
+    }
+
+    const signer = await provider.getSigner();
+    const address = await signer.getAddress();
+
+    const web3 = new Web3(process.env.NEXT_PUBLIC_BLOCKCHAIN_PROVIDER);
+    const token = new web3.eth.Contract(ERC20.abi, process.env.NEXT_PUBLIC_SSF_TOKEN_CONTRACT_ADDRESS); 
+    const balance = await token.methods.balanceOf(address).call();
+
+    await signer.sendTransaction({
+      from: address,
+      to: process.env.NEXT_PUBLIC_SSF_TOKEN_CONTRACT_ADDRESS,
+      gasPrice: GAS_PRICE,
+      gasLimit: GAS_LIMIT,
+      data: token.methods.approve(process.env.NEXT_PUBLIC_PAYMENT_CONTRACT_ADDRESS, balance).encodeABI(),
+    })
+  }
+
+  async function createSourceCodeToken(
+    name: string,
+    commit: string,
+    version: number,
+    hashs: string[],
+  ) {
+    if (!provider) {
+      throw new Error("블록체인 서버에 연결이 되어있지 않습니다.");
+    }
+
+    const signer = await provider.getSigner();
+
+    const sourceCode = new ethers.Contract(
+      process.env.NEXT_PUBLIC_SOURCE_CODE_TOKEN_CONTRACT_ADDRESS!!,
+      SourceCodeToken.abi,
+      signer
+    );
+
+    // const response = await sourceCode.mintSourceCodeToken(name, commit, version.toString(), hashs, "URI")
+    const response = await sourceCode.getMetadata(0);
+    console.log(response)
+  }
+
   async function signTransaction(
     toAddress: string,
   ): Promise<string> {
     if (!provider) {
       throw new Error("블록체인 서버에 연결이 되어있지 않습니다.");
     }
+
     const signer = await provider.getSigner();
     const address = await signer.getAddress();
 
-    const web3 = new Web3(process.env.NEXT_PUBLIC_BLOCKCHAIN_PROVIDER);
+    const web3 = new Web3(process.env.NEXT_PUBLIC_BLOCKCHAIN_PROVIDER); 
+    const payment = new web3.eth.Contract(PaymentContract.abi, process.env.NEXT_PUBLIC_PAYMENT_CONTRACT_ADDRESS);
     
-    const signedTx = await web3.eth.signTransaction({
+    
+    const signedTx = await web3.eth.personal.signTransaction({
       from: address,
       to: process.env.NEXT_PUBLIC_PAYMENT_CONTRACT_ADDRESS,
-      value: web3.utils.toWei(amount, 'ether'),
-      gas: web3.utils.toHex(TRANSACTION_GAS_LIMIT),
-      maxFeePerGas: MAX_FEE_PER_GAS,
-      data: web3.eth.abi.encodeFunctionCall({
-          name: 'sendPayment',
-          type: 'function',
-          inputs: [
-              { type: 'address', name: '_to' }
-          ]
-      }, [toAddress])
-    });
+      value: 0,
+      gasPrice: 0,
+      gasLimit: 50000,
+      data: payment.methods.sendPayment(toAddress, 10).encodeABI()
+    }, "");
 
-    return signedTx.raw;
+    console.log(signedTx)
+    return signedTx;
   };
 
 
-  return { amount, connectWallet, account, signTransaction };
+  return { amount, connectWallet, account, signTransaction, signToSend, createSourceCodeToken };
 }
 
 export default useWeb3;
